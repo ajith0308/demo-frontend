abb165b29258262acd8c683772bf75d89bf29d84
BabishaV <babisha1581@gmail.com>
2023-09-27 09:31:26 +0530

    demo

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
